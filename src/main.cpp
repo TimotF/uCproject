22e572b6303642196564ba3c3ccb8b793d0b19f0
Timothé Frignac <timothe@purecontrol.com>
2019-03-01 15:57:19 +0000

    Added pin declaration and init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include "pin.h"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  // CNY70 pin init
+  pinMode(PIN_EN_3V3, OUTPUT);
+  digitalWrite(PIN_EN_3V3, LOW);
+
+  // ENDSTOP pin init
+  pinMode(PIN_ENDSTOP_1, INPUT_PULLUP);
+  pinMode(PIN_ENDSTOP_2, INPUT_PULLUP);
+
+  // MOTOR pin init
+  pinMode(PIN_MOT_SLEEP, OUTPUT);
+  digitalWrite(PIN_MOT_SLEEP, LOW);
+  pinMode(PIN_MOT_FAULT, INPUT_PULLUP);
+  pinMode(PIN_MOT_STEER1, OUTPUT);
+  pinMode(PIN_MOT_STEER2, OUTPUT);
+  pinMode(PIN_MOT_PROP1, OUTPUT);
+  pinMode(PIN_MOT_PROP2, OUTPUT);
+
+  // OTHER pin init
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(PIN_LED, !digitalRead(PIN_LED));
+  delay(500);
 }
---
+++ src/pin.h
@@ -0,0 +1,40 @@
+#ifndef PIN_H
+#define PIN_H
+
+// CNY70
+#define PIN_EN_3V3      18
+#define PIN_CNY70_1     36
+#define PIN_CNY70_2     39
+#define PIN_CNY70_3     34
+#define PIN_CNY70_4     35
+#define PIN_CNY70_5     32
+#define PIN_CNY70_6     25
+#define PIN_CNY70_7     26
+#define PIN_CNY70_8     27
+#define PIN_CNY70_9     14
+#define PIN_CNY70_10    12 
+
+// ENDSTOP
+#define PIN_ENDSTOP_1   33
+#define PIN_ENDSTOP_2   16
+
+// BATTERY
+#define PIN_VBAT        13
+
+// I2C
+#define PIN_SCL         22
+#define PIN_SDA         21
+
+// MOTOR
+#define PIN_MOT_SLEEP   19
+#define PIN_MOT_FAULT   23
+#define PIN_MOT_STEER1  4
+#define PIN_MOT_STEER2  0
+#define PIN_MOT_PROP1   2
+#define PIN_MOT_PROP2   15
+
+// MISCELLANEOUS
+#define PIN_LED         17
+#define PIN_BUTTON      5
+
+#endif
