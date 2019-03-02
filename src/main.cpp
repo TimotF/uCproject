e59eb00932f409e3ab7e5953126b2e18ee7a1c92
Timothé Frignac <timothe@purecontrol.com>
2019-03-02 00:03:41 +0000

    First test program

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #define BACKGROUND_TASK_PRIORITY 0
 #define BACKGROUND_TASK_CORE 0
 
+#define CH_MOT_PROP1 2
+#define CH_MOT_PROP2 3
+#define PWM_FREQUENCY 500
+#define PWM_RESOLUTION 8
+
 /**
  * This task is used to run all background process
  */
@@ -34,6 +39,10 @@ void setup()
   pinMode(PIN_MOT_STEER2, OUTPUT);
   pinMode(PIN_MOT_PROP1, OUTPUT);
   pinMode(PIN_MOT_PROP2, OUTPUT);
+  ledcSetup(CH_MOT_PROP1, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcSetup(CH_MOT_PROP2, PWM_FREQUENCY, PWM_RESOLUTION);
+  ledcAttachPin(PIN_MOT_PROP1, CH_MOT_PROP1);
+  ledcAttachPin(PIN_MOT_PROP2, CH_MOT_PROP2);
 
   // OTHER pin init
   pinMode(PIN_LED, OUTPUT);
@@ -49,8 +58,55 @@ void setup()
       BACKGROUND_TASK_PRIORITY, /* Priority of the task */
       NULL,                     /* Task handle. */
       BACKGROUND_TASK_CORE);    /* Core where the task should run */
+
+  digitalWrite(PIN_MOT_SLEEP, HIGH);
+}
+
+void left()
+{
+  digitalWrite(PIN_MOT_STEER1, HIGH);
+  digitalWrite(PIN_MOT_STEER2, LOW);
+}
+
+void right()
+{
+  digitalWrite(PIN_MOT_STEER1, LOW);
+  digitalWrite(PIN_MOT_STEER2, HIGH);
+}
+
+void straight()
+{
+  digitalWrite(PIN_MOT_STEER1, LOW);
+  digitalWrite(PIN_MOT_STEER2, LOW);
+}
+
+void forward(byte speed)
+{
+  ledcWrite(CH_MOT_PROP1, speed);
+  ledcWrite(CH_MOT_PROP2, 0);
+}
+
+void backward(byte speed)
+{
+  ledcWrite(CH_MOT_PROP1, 0);
+  ledcWrite(CH_MOT_PROP2, speed);
 }
 
 void loop()
 {
+  forward(128);
+  straight();
+  delay(1000);
+
+  backward(128);
+  right();
+  delay(1000);
+
+  forward(128);
+  straight();
+  delay(1000);
+
+  backward(128);
+  left();
+  delay(1000);
 }
